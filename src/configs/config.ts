1913a187411505289a9691b2ba3b4b6c94fc3cc8
sohanemon <m.sohanemon@gmail.com>
2023-12-13 15:48:22 +0600

    chore(husky): Update pre-commit script
    The pre-commit script has been updated to run linting and code
    formatting before committing by replacing the existing "npm test"
    command with "npm run lint && npm run format:write". This change ensures
    that the committed code adheres to the defined coding standards and
    formatting rules.

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,7 +1,7 @@
 const config = {
   version: '1.0.0',
   name: 'express-ts',
-  description: 'A minimal typescript & express starter',
+  description: 'Simple express-ts starter with file based routing',
 
   nodeEnv: process.env.NODE_ENV ?? 'development',
   port: process.env.PORT ?? 3000,
