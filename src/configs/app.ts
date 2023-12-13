7331c102912b1e69655397895aebc3a1a9662856
sohanemon <m.sohanemon@gmail.com>
2023-12-13 12:23:43 +0600

    bug: tsconfig

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -2,33 +2,28 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
+import errorHandler from 'middleware/errorHandler.js';
+import fourOhFour from 'middleware/fourOhFour.js';
 import morgan from 'morgan';
+import root from 'routes/root.js';
 
-import config from './config';
-import errorHandler from './middleware/errorHandler';
-import fourOhFour from './middleware/fourOhFour';
-import root from './routes/root';
-
-const app = express()
+const app = express();
 
 // Apply most middleware first
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(cors({
-    // @ts-ignore no-implicit-any
-    origin: config.clientCorsOrigins[config.nodeEnv] ?? '*'
-}))
+app.use(cors());
 
-app.use(helmet())
-app.use(morgan('tiny'))
+app.use(helmet());
+app.use(morgan('tiny'));
 
 // Apply routes before error handling
-app.use('/', root)
+app.use('/', root);
 
 // Apply error handling last
-app.use(fourOhFour)
-app.use(errorHandler)
+app.use(fourOhFour);
+app.use(errorHandler);
 
-export default app
+export default app;
--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,18 +1,16 @@
-import packageJson from '../package.json';
-
 const config = {
-    version: packageJson.version,
-    name: packageJson.name,
-    description: packageJson.description,
+  version: '1.0.0',
+  name: 'express-ts',
+  description: 'A minimal typescript & express starter',
 
-    nodeEnv: process.env.NODE_ENV ?? 'development',
-    port: process.env.PORT ?? 3000,
+  nodeEnv: process.env.NODE_ENV ?? 'development',
+  port: process.env.PORT ?? 3000,
 
-    clientCorsOrigins: {
-        'test': process.env.DEV_ORIGIN ?? '*',
-        'development': process.env.DEV_ORIGIN ?? '*',
-        'production': process.env.PROD_ORIGIN ?? 'none'
-    }
-}
+  clientCorsOrigins: {
+    test: process.env.DEV_ORIGIN ?? '*',
+    development: process.env.DEV_ORIGIN ?? '*',
+    production: process.env.PROD_ORIGIN ?? 'none',
+  },
+};
 
-export default config
+export default config;
