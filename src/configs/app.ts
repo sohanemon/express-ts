86722cb9fd1399b546579340c017cd02b14ec212
sohanemon <m.sohanemon@gmail.com>
2023-12-13 15:41:42 +0600

    fix: minor issues

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import routeHandlers from '../handler.js';
-import { setupErrorHandlers } from '../middleware/errorHandler.js';
-import { setupGlobalMiddlewares } from '../middleware/global.js';
+import { setupErrorHandlers } from '../middleware/error-handlers.js';
+import { setupHelpers } from '../middleware/helpers.js';
 
 const app = express();
 
 // middleware must be at first
-setupGlobalMiddlewares(app);
+setupHelpers(app);
 
 // routes must be at middle
 await routeHandlers(app);
