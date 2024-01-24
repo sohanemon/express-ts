0bd3aea766a5c9f629382f9326955b9045d86b0e
sohanemon <m.sohanemon@gmail.com>
2024-01-24 22:04:24 +0600

    feat(middleware): Update express middleware

--- src/middleware/helpers.ts
+++ src/middleware/helpers.ts
@@ -1,16 +1,16 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express from 'express';
+import { json, static as staticFolder, urlencoded, Application } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
-export const setupHelpers = (app: express.Application) => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cookieParser());
-
-  app.use(cors());
-
-  app.use(helmet());
-  app.use(morgan('tiny'));
+export const setupHelpers = (app: Application) => {
+  app
+    .use(json())
+    .use(staticFolder('public'))
+    .use(urlencoded({ extended: true }))
+    .use(cookieParser())
+    .use(cors())
+    .use(helmet())
+    .use(morgan('tiny'));
 };
